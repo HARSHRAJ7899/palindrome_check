9872650976f2644912a13cb1eff993d3df0c5d2d
Harsh raj <harshraj@Harshs-MacBook-Air.local>
2022-11-02 11:36:37 +0530

    changed

--- palindrome.js
+++ palindrome.js
@@ -1,25 +1,35 @@
-// program to check if the string is palindrome or not
-
-function checkPalindrome(string) {
-
-    // find the length of a string
-    const len = string.length;
-
-    // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
-
-        // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
-            return 'It is not a palindrome';
-        }
+function check_palindrome(str) {
+  let j = str.length - 1;
+  for (let i = 0; i < j / 2; i++) {
+    let x = str[i]; //forward character
+    let y = str[j - i]; //backward character
+    if (x != y) {
+      // return false if string not match
+      return false;
     }
-    return 'It is a palindrome';
+  }
+  /// return true if string is palindrome
+  return true;
 }
 
-// take input
-const string = prompt('Enter a string: ');
-
-// call the function
-const value = checkPalindrome(string);
-
-console.log(value);
+//function that print output is string is palindrome
+function is_palindrome(str) {
+  // variable that is true if string is palindrome
+  let ans = check_palindrome(str);
+  //condition checking ans is true or not
+  if (ans == true) {
+    console.log("passed string is palindrome ");
+  } else {
+    console.log("passed string not a palindrome");
+  }
+}
+//take input from terminal
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.question('Enter string : ', name => {
+    let test = name;
+    is_palindrome(test);
+    readline.close();
+});// take input
